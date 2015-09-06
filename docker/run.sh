db1ba7fcd44c143666afebd078a4da0a2bed08f8
ruffsl <roxfoxpox@gmail.com>
2015-09-06 18:43:06 -0400

    Adding $USER to run script

--- docker/run.sh
+++ docker/run.sh
@@ -5,7 +5,7 @@ docker run \
 	--env="DISPLAY" \
 	--env="QT_X11_NO_MITSHM=1" \
 	--volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-	--volume="/home/ruffsl/Desktop/caffe:/root/" \
+	--volume="/home/${USER}/Desktop/caffe:/root/Desktop" \
 	--device /dev/nvidia0:/dev/nvidia0 \
 	--device /dev/nvidiactl:/dev/nvidiactl \
 	--device /dev/bus/usb:/dev/bus/usb \
